a590ddc4708bf88c9394cd77883e61b1d7b6e882
herufredi85 <48423946+herufredi85@users.noreply.github.com>
2019-03-11 09:37:22 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import Routes from './src/Routes';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Routes/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+  }
+});
